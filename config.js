4bead2ba74ffec94a6b49b1afe3690b869608f55
Yassine <Apple@192.168.1.4>
2024-07-06 18:30:19 +0100

    ui changes

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoUrl: process.env.MONGO_URL,
-    azureStorageAccount: process.env.AZURE_STORAGE_ACCOUNT,
-    azureStorageKey: process.env.AZURE_STORAGE_KEY,
-    containerName: 'audiofiles'
-};
